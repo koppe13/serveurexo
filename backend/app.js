451b47f615e49b00f46937c2948c46c4fc6a6cdf
koppe13 <phenixairsoft2@gmail.com>
2025-01-12 20:55:33 +0100

    test push 2

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Thing');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const path = require('path');
 
 mongoose.connect('mongodb+srv://phenixairsoft2:Babylon1974@koppe.hui2h.mongodb.net/?retryWrites=true&w=majority&appName=koppe',
   { useNewUrlParser: true,
@@ -10,30 +12,17 @@ mongoose.connect('mongodb+srv://phenixairsoft2:Babylon1974@koppe.hui2h.mongodb.n
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-app.use(express.json());
+  app.use(express.json());
 
-app.use((req, res, next) => {
+  app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
 
-  app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  
-
-  app.use('/api/stuff', (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  });
+  app.use('/api/stuff', stuffRoutes);
+  app.use('/api/auth', userRoutes);
+  app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
---
+++ backend/routes/stuff.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const auth = require ('../Middleware/auth');
+const multer = require('../Middleware/multer-config');
+const stuffCtrl = require('../controllers/stuff');
+
+
+
+router.get('/', auth, stuffCtrl.getAllThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+
+
+  module.exports = router;
